c744a45b705a390e091b08156656ff1b1c13a277
anghelr14 <72480694+anghelr14@users.noreply.github.com>
2022-01-19 16:47:25 -0600

    Servidor listo

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+
+/*===== Settings =====*/
+app.set('port', process.env.PORT || 9000);
+
+
+/*===== Middlewares =====*/
+app.use(express.json());
+
+/*===== Routes =====*/
+app.get('/', (req, res) => {
+    res.send('Bienveido a la api para Alexa')
+});
+
+
+
+
+/*===== Starting server =====*/
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
