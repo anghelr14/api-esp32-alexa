4b710a4180a6646a8906ce020f59190c7701b0a0
anghelr14 <72480694+anghelr14@users.noreply.github.com>
2022-01-19 18:06:24 -0600

    Base de datos actualizada

--- src/database.js
+++ src/database.js
@@ -1,20 +1,16 @@
 const mysql = require('mysql');
 const e = require("express");
 
-const mysqlConnection = mysql.createConnection({
+var pool = mysql.createPool({
     host:'us-cdbr-east-05.cleardb.net',
     user:'b4055405d45cb7',
     password:'e9ae2616',
     database:'heroku_ed507f11fb8ee5e'
 });
 
-mysqlConnection.connect((e)=>{
-    if (e){
-        console.log(e);
-        return;
-    } else {
-        console.log(`Db is connected`);
-    }
+pool.getConnection(function(err, connection) {
+    console.log(err);
+    connection.destroy();
 });
 
-module.exports = mysqlConnection;
+module.exports = pool;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const mysqlConnection = require('../database.js');
+const pool = require('../database.js');
 
 router.get('/',(req, res) => {
   const query = 'SELECT * FROM users';
-  mysqlConnection.query(query,(err,rows,fields) =>{
+    pool.query(query,(err,rows,fields) =>{
       if (!err){
           res.json(rows);
       } else{
@@ -17,7 +17,7 @@ router.get('/',(req, res) => {
 router.get('/:id', (req, res) => {
     const {id} = req.params;
     const query = 'SELECT * FROM users WHERE id = ?';
-    mysqlConnection.query(query,[id],(err,rows,fields)=>{
+    pool.query(query,[id],(err,rows,fields)=>{
        if (!err){
            res.json(rows[0]);
        } else{
